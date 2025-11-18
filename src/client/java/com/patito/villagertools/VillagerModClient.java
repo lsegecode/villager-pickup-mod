679f6af6401800d6373f4eb7e430f8d5e7614e65
Lucas Sanabria <sanabrialucas97@gmail.com>
2025-11-18 00:00:22 -0300

    added example mod

---
+++ src/client/java/com/patito/villagertools/VillagerModClient.java
@@ -0,0 +1,10 @@
+package com.patito.villagertools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VillagerModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
